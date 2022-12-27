7f7e336dae6fc040ebbd7b599c2ad80c7b77a63d
juniorovi <mdtarikulislam258000@gmail.com>
2022-12-27 12:42:58 +0600

    rest country api

---
+++ js/rest-country-api.js
@@ -0,0 +1,21 @@
+const loadcountries = () =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+const displayCountries =  countries => {
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML;
+}
+
+const getCountryHTML = country => {
+    return `
+    <div>
+        <h2>${country.name.common}
+    </div>
+    `
+}
+
+loadcountries();
